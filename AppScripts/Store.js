15bfc5e2f3636dd8d29b0a6f90287094f79f6217
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-06-17 13:46:08 +0300

    first commit

---
+++ AppScripts/Store.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Maayan on 6/17/2017.
+ */
+(function () {
+    'use strict';
+
+    var app = angular.module('Store', []);
+    app.controller('mainController', ['$http', function ($http) {
+        var vm = this;
+
+
+    }]);
+
+})();
